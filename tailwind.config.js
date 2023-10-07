238985e3d43f421dbb0801a7017c2d9a3184e726
bonane <ndayishimyebonane@gmail.com>
2023-10-08 02:01:22 +0300

    portfolio v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,44 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*.{js,jsx}'];
-export const mode = 'jit';
-export const theme = {
-  extend: {
-    colors: {
-      primary: '#050816',
-      secondary: '#aaa6c3',
-      tertiary: '#151030',
-      'black-100': '#100d25',
-      'black-200': '#090325',
-      'white-100': '#f3f3f3',
-    },
-    boxShadow: {
-      card: '0px 35px 120px -15px #211e35',
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        blue: '#2CBCE9',
+        red: '#DC4492',
+        yellow: '#FDCC49',
+        grey: '#ededed',
+        'deep-blue': '#010026',
+        'dark-grey': '#757557',
+        'opaque-black': 'rgba(0,0,0,0.35)',
+      },
+      backgroundImage: (theme) => ({
+        'gradient-rainbow':
+          'linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)',
+
+        'gradient-rainblue':
+          'linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)',
+      }),
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+        opensans: ['Open Sans', 'sans-serif'],
+      },
+      content: {
+        brush: "url('./assets/brush.png')",
+        person1: "url('./assets/person-1.png')",
+        person2: "url('./assets/person-2.png')",
+        person3: "url('./assets/person-3.png')",
+      },
     },
     screens: {
-      xs: '450px',
-    },
-    backgroundImage: {
-      'hero-pattern': "url('/src/assets/herobg.png')",
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
     },
   },
+  plugins: [],
 };
-export const plugins = [];
